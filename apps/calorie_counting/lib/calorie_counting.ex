24bcd010091eff446c5d9461193444ef07f200bd
FranPek <pedreirafran@gmail.com>
2022-12-01 22:56:47 +0100

    Calorie counting #Day1

--- apps/calorie_counting/lib/calorie_counting.ex
+++ apps/calorie_counting/lib/calorie_counting.ex
@@ -4,15 +4,42 @@ defmodule CalorieCounting do
   """
 
   @doc """
-  Hello world.
+  count_calories
 
   ## Examples
 
-      iex> CalorieCounting.hello()
-      :world
+      iex> CalorieCounting.count_calories("./apps/calorie_counting/files/example.txt")
+      2400
+      iex> CalorieCounting.count_calories("./apps/calorie_counting/files/example.txt")
+      66306
+
+      iex> CalorieCounting.count_calories("./apps/calorie_counting/files/sample.txt", 3)
+      4500
+      iex> CalorieCounting.count_calories("./apps/calorie_counting/files/sample.txt", 3)
+      195292
 
   """
-  def hello do
-    :world
+
+  ## FIRST IMPLEMENTATION
+  #  def find_most_calories_elf(path) do
+  #    File.read!(path)
+  #    |> String.split("\r\n\r\n")
+  #    |> Enum.map(&count_elf_calories/1)
+  #    |> Enum.max()
+  #  end
+
+  def count_calories(path, take \\ 1) do
+    File.read!(path)
+    |> String.split("\r\n\r\n")
+    |> Enum.map(&count_elf_calories/1)
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.take(take)
+    |> Enum.sum()
+  end
+
+  defp count_elf_calories(elf) do
+    String.split(elf, "\r\n")
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
   end
 end
